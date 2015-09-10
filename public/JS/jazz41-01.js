b441fe315491803f3473340b07ec232639a1c115
Thomas L <thomasl@stardevsoft.com>
2015-09-10 09:53:18 -0400

    js mod for me

--- public/JS/jazz41-01.js
+++ public/JS/jazz41-01.js
@@ -245,7 +245,10 @@ function showRadio() {
 $(document).ready(function () {   
   visibleDIV = 1;
 
+/* TLL 2015/09/10 */
+/* revert as FF doesn't work */
 /*  $('audio').mediaelementplayer(); */
+/* TLL 2015/09/10 */
 
 /* TLL 2015/02/04 */
   HideCalendar();
